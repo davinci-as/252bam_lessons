5f50e8953f8a658c352fc3ac883157fc08fe6283
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-08-28 21:42:48 -0300

    refactor: mover strings a variables

--- app/src/main/java/ar/edu/davinci/a252b_am_lessons/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("testing-app", "Main Activity running");
+        Log.d("testing-app", getString(R.string.testing_log_message));
     }
 }
