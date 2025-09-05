3644ae317e5be24b6cfb0ebfe369e99a4af2bd31
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-09-04 22:18:07 -0300

    feat: agregar vistas dinamicamente

--- app/src/main/java/ar/edu/davinci/a252b_am_lessons/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/MainActivity.java
@@ -3,13 +3,28 @@ package ar.edu.davinci.a252b_am_lessons;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         Log.d("testing-app", getString(R.string.testing_log_message));
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView textView = new TextView(this);
+        textView.setText(R.string.welcome_message);
+        linearLayout.addView(textView);
+
+        Button button = new Button(this);
+        button.setText(R.string.button_message);
+        linearLayout.addView(button);
+
+        setContentView(linearLayout);
     }
 }
