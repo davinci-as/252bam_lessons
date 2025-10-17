08bf0196e7a9be28c6e2b81dde7e20c966c77cbd
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-16 22:43:09 -0300

    feat: descargar y formatear imagen como bitmap

--- app/src/main/java/ar/edu/davinci/a252b_am_lessons/ImageDownloader.java
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/ImageDownloader.java
@@ -1,14 +1,29 @@
 package ar.edu.davinci.a252b_am_lessons;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ImageDownloader extends AsyncTask<String, Integer, Bitmap> {
     @Override
     protected Bitmap doInBackground(String... strings) {
-        Log.i("async-task", "working doInBackground");
-        return null;
+        String url = strings[0];
+        Log.i("async-task", url);
+        try {
+            URL request = new URL(url);
+            InputStream inputStream = (InputStream) request.getContent();
+            return BitmapFactory.decodeStream(inputStream);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
