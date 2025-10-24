acef1a10df61027c5d607bd0228bc310c7051ab9
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-23 22:30:37 -0300

    feat: agregar consulta GET a endpoint

--- app/src/main/java/ar/edu/davinci/a252b_am_lessons/ApiRequest.java
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/ApiRequest.java
@@ -3,6 +3,10 @@ package ar.edu.davinci.a252b_am_lessons;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.OkHttpClient;
@@ -37,5 +41,25 @@ public class ApiRequest extends AsyncTask<String, Integer, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        try {
+            JSONObject alienRick = new JSONObject(s);
+            //JSONObject info = jsonObject.getJSONObject("info");
+            //JSONArray results = jsonObject.getJSONArray("results");
+            //JSONObject alienRick = results.getJSONObject(14);
+            String alienRickName = "default";
+            String alienRickUrl = "https://placeholder.com/20x20";
+            if(alienRick.has("name")) {
+                alienRickName = alienRick.getString("name");
+            }
+            if(alienRick.has("image")) {
+                alienRickUrl = alienRick.getString("image");
+            }
+
+
+            Log.i("alien-rick", String.format("%s %s", alienRickName, alienRickUrl));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
