055160b47b104c5c94067f8c10c2ee3645a3e271
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-23 21:59:54 -0300

    feat: agregar consulta a url

---
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/ApiRequest.java
@@ -0,0 +1,41 @@
+package ar.edu.davinci.a252b_am_lessons;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ApiRequest extends AsyncTask<String, Integer, String> {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String url = strings[0];
+        try {
+            String objectJson = run(url);
+            Log.i("json-object", objectJson);
+            return objectJson;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
