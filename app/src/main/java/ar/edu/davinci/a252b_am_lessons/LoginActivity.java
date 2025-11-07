bd1260ae459021284a15eea9dd6bbf88cbbfb11b
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-06 22:18:47 -0300

    feat: agregar formulario de login

--- app/src/main/java/ar/edu/davinci/a252b_am_lessons/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/a252b_am_lessons/LoginActivity.java
@@ -1,14 +1,47 @@
 package ar.edu.davinci.a252b_am_lessons;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    public void loginUser(View v) {
+        EditText emailInput = findViewById(R.id.email);
+        EditText passwordInput = findViewById(R.id.password);
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.i("firebase-auth", "login completado");
+                        Log.i("firebase-auth", email);
+                        Log.i("firebase-auth", password);
+                        if(task.isSuccessful()) {
+                            //TODO: redireccionar a Main
+                        }
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
     }
 }
